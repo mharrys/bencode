2db2269132309795668f3e451436891726788d45
Mattias Harrysson <mathar@kth.se>
2016-08-13 13:25:17 +0200

    implement decoding of strings

--- src/main/scala/Bencode.scala
+++ src/main/scala/Bencode.scala
@@ -8,9 +8,16 @@ case class BList(value: Seq[BValue]) extends BValue;
 case class BDict(value: Map[String, BValue]) extends BValue;
 
 object Bencode {
-  def decode(data: String): Either[String, BValue] = data match {
-    case d if d.startsWith("i") => decodeInt(data)
-    case _ => Left("not implemented")
+  def decode(data: String): Either[String, BValue] = {
+    lazy val dataStrStart = data takeWhile(Character.isDigit(_))
+    data match {
+      case d if d startsWith "i" =>
+        decodeInt(data)
+      case d if dataStrStart.length > 0 =>
+        decodeStr(data, dataStrStart)
+      case _ =>
+        Left("not implemented")
+    }
   }
 
   private def decodeInt(dataInt: String): Either[String, BInt] = {
@@ -26,5 +33,16 @@ object Bencode {
       case d => parse(data drop 1, acc :+ d.head)
     }
     parse(dataInt drop 1, Seq())
+  }
+
+  private def decodeStr(dataStr: String, dataStrStart: String): Either[String, BStr] = {
+    val strLength = dataStrStart.toInt
+    val dataStrTail = dataStr drop dataStrStart.length
+    val dataStrLength = dataStrTail.length - 1 // -1 accounts for ":"
+    if (dataStrTail.startsWith(":") && dataStrLength >= strLength) {
+      val strContent = dataStrTail drop 1 take strLength
+      Right(BStr(strContent))
+    } else
+      Left("Unexpected ending while parsing string")
   }
 }
--- src/test/scala/BencodeSpec.scala
+++ src/test/scala/BencodeSpec.scala
@@ -1,8 +1,9 @@
 import org.scalatest.{FunSuite, Matchers}
-import org.scalatest.prop.Checkers
+import org.scalatest.prop.{Checkers, GeneratorDrivenPropertyChecks}
 
-class BencodeSuite extends FunSuite with Checkers with Matchers {
-
+class BencodeSuite extends FunSuite with Checkers
+                                    with Matchers
+                                    with GeneratorDrivenPropertyChecks {
   test("can decode integer") {
     check((n: Int) => {
       Bencode.decode(s"i${n}e") == Right(BInt(n)) &&
@@ -12,5 +13,34 @@ class BencodeSuite extends FunSuite with Checkers with Matchers {
     check((s: String) => {
       Bencode.decode(s"i${s}e").isLeft
     })
+  }
+
+  test("can decode string") {
+    // edge case
+    Bencode.decode(s"0:") shouldEqual (Right(BStr("")))
+    check((s: String) => {
+      Bencode.decode(s"${s.length}:${s}") == Right(BStr(s)) &&
+      Bencode.decode(s"${s.length}:${s}${s}") == Right(BStr(s)) &&
+      Bencode.decode(s"${s.length}${s}").isLeft
+    })
+    // test only digits or with appended separator (including negative numbers)
+    forAll { (n: Int) =>
+      whenever (n > 0) {
+        Bencode.decode(s"${n}").isLeft
+        Bencode.decode(s"${n}:").isLeft
+      }
+    }
+    // test strings that are too short
+    forAll { (s: String, n: Int) =>
+      whenever (s.length > 0 && n > 0) {
+        Bencode.decode(s"${s.length}:${s.drop(n)}").isLeft
+      }
+    }
+    // test different separators
+    forAll { (s: String, c: Char) =>
+      whenever (c != ":") {
+        Bencode.decode(s"${s.length}${c}${s}").isLeft
+      }
+    }
   }
 }
