0bc14f3bff64d963a60535ce961252386f491806
Mattias Harrysson <mathar@kth.se>
2016-08-13 12:28:08 +0200

    add property test for invalid strings

--- src/test/scala/BencodeSpec.scala
+++ src/test/scala/BencodeSpec.scala
@@ -9,5 +9,8 @@ class BencodeSuite extends FunSuite with Checkers with Matchers {
       Bencode.decode(s"${n}e").isLeft &&
       Bencode.decode(s"i${n}").isLeft
     })
+    check((s: String) => {
+      Bencode.decode(s"i${s}e").isLeft
+    })
   }
 }
