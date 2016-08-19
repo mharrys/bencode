c5fd81e2ca9ae0668d8cfa5f62bdc762aa908805
Mattias Harrysson <mathar@kth.se>
2016-08-19 17:48:47 +0200

    override toString on BValues

--- src/main/scala/bencode/BValue.scala
+++ src/main/scala/bencode/BValue.scala
@@ -2,10 +2,21 @@ package bencode
 
 sealed trait BValue;
 
-case class BInt(value: Int) extends BValue;
-case class BStr(value: String) extends BValue;
-case class BList(value: Vector[BValue]) extends BValue;
-case class BDict(value: Map[String, BValue]) extends BValue;
+case class BInt(value: Int) extends BValue {
+  override def toString = value.toString
+}
+
+case class BStr(value: String) extends BValue {
+  override def toString = value
+}
+
+case class BList(value: Vector[BValue]) extends BValue {
+  override def toString = value.mkString("[", " ", "]")
+}
+
+case class BDict(value: Map[String, BValue]) extends BValue {
+  override def toString = value.mkString("{", " ", "}")
+}
 
 object BList {
   def apply(values: BValue*) = new BList(Vector(values:_*))
